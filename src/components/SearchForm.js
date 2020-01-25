605115a9cd390e4d20a025f43202ce913327f181
AdamMaley <adamemaley@yahoo.com>
2020-01-24 21:15:19 -0500

    still working on search form.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,11 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { } from 'formik';
+import { useFormik, withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
 export default function SearchForm() {
  
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <section className="search-form">
-     // Add a search form here
+      <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input
+        id="firstName"
+        name="firstName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.firstName}
+      />
+      <label htmlFor="lastName">Last Name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastName}
+      />
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Submit</button>
+    </form>
     </section>
   );
 }
