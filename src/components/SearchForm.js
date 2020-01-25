de4601a37fc98f0bae5e4876c574bfd802c97076
AdamMaley <adamemaley@yahoo.com>
2020-01-24 21:57:11 -0500

    time up, can't figure out Formik or how to incooperate a .filter for Searchbar.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,43 +6,22 @@ export default function SearchForm() {
  
   const formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-    },
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
+      search: ''
     },
   });
 
   return (
     <section className="search-form">
       <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
+      <label htmlFor="firstName">Search Character: </label>
       <input
-        id="firstName"
-        name="firstName"
+        id="search"
+        name="search"
         type="text"
         onChange={formik.handleChange}
         value={formik.values.firstName}
       />
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.lastName}
-      />
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      <button type="submit">Submit</button>
+      <button type="submit">Search</button>
     </form>
     </section>
   );
